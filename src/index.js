8f7f9f8d4eb136859a18680a2f365d3d683c16c2
Gabriel Matthew Labariento <gabriel.matthew.labariento@student.ateneo.edu>
2025-07-03 13:38:04 +0800

    Finished weather app

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ const address = document.getElementById("address")
 const temp = document.getElementById("temp");
 const description = document.getElementById("desc")
 const mainIcon = document.getElementById("main-icon");
-
+let isFahrenheit = true;
+const toggleButton = document.querySelector(".toggle-btn")
+ const daysOfWeek = ["SUN", "MON", "TUES", "WED", "THURS", "FRI", "SAT"];
 
 locationForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -48,14 +50,15 @@ async function processData() {
 
 async function displayData() {
     try {
+        toggleButton.textContent = isFahrenheit ? "°F" : "°C"
         const processedData = await processData();
         console.log(processedData)
         // Capitalize first letter
     
         address.textContent = processedData.resolvedAddress.charAt(0).toUpperCase() + processedData.resolvedAddress.slice(1);
         description.textContent = processedData.currentConditions.conditions;
-        temp.textContent = processedData.currentConditions.temp + "F";
-        
+        temp.textContent = isFahrenheit ? processedData.currentConditions.temp + toggleButton.textContent : 
+                                            ((parseFloat(processedData.currentConditions.temp) - 32) * 5/9).toFixed(1) + toggleButton.textContent;
         displayIcon(mainIcon, processedData.currentConditions.icon);
         
         weatherInfoGrid.appendChild(address);
@@ -68,9 +71,9 @@ async function displayData() {
             const bubble = bubbles[index];
             bubble.querySelector(`.icon-${index + 1}`).hasChildNodes[0] && bubble.querySelector(`.icon-${index + 1}`).removeChild(bubble.querySelector(`.icon-${index + 1}`).firstChild);
             displayIcon(bubble.querySelector(`.icon-${index + 1}`), day.icon);
-            bubble.querySelector(`.day-${index + 1}`).textContent = day.datetime;
-            bubble.querySelector(`.temp-${index + 1}`).textContent = day.temp + "F";
-            // bubble.style.display = "block"; // Show the bubble
+            bubble.querySelector(`.day-${index + 1}`).textContent = daysOfWeek[new Date(day.datetimeEpoch).getDay()];
+            bubble.querySelector(`.temp-${index + 1}`).textContent = isFahrenheit ? day.temp + toggleButton.textContent : 
+                                                                                    ((parseFloat(day.temp) - 32) * 5/9).toFixed(1) + toggleButton.textContent;
             forecastGrid.appendChild(bubble);
         });
         weatherResultContainer.appendChild(forecastGrid)
@@ -83,15 +86,23 @@ async function displayData() {
 
 async function displayIcon(parentContainer, iconCode) {
     try {
-        const icon = document.createElement("img")
+        const icon = (parentContainer.querySelector("img")) ? parentContainer.querySelector("img") : document.createElement("img")
         import (`./icons/${iconCode}.svg`).then((module) => {
             icon.src = module.default
         })
+
+        
+        
         parentContainer.appendChild(icon);
     } catch (error) {
         console.log("Error in displayIcon", error)
     }
 }
+
+toggleButton.addEventListener("click", () => {
+    isFahrenheit = !isFahrenheit
+    displayData();
+})
 
 displayData();
 
